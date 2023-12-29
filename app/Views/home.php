9d5d92d1f4ed83ce0d04149ad4e8193945677010
Yasser <aitelghariyasser@gmail.com>
2023-12-29 15:51:09 +0100

    add navbar and footer

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <?php require "../../public/assets/head_hf.php"; ?>
 </head>
+
 <body>
-    <h1>Welcome Home</h1>
+
+
+    <?php require "includes/navbar.php"; ?>
+    <section class="main-home">
+        <div class="main-hero">
+            <h1 class="hero-header">Exciting moments<br>await You</h1>
+        </div>
+        <div style="text-align:center;">
+            <div class="search-section">
+                <input name="search" type="text" placeholder="search...">
+                <img src="images/calendar_today.png" id="datePicker" alt="date">
+                <button type="button" class="btn btn-success">
+                    <img src="images/search_icon.png" alt="search">
+                    <span>Search</span>
+                </button>
+            </div>
+        </div>
+    </section>
+    <?php require "includes/footer.php"; ?>
 </body>
+
 </html>
---
+++ public/assets/head_hf.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css_bootstrap/bootstrap.min.css">
+    <script src=" ../../public/js_bootstrap/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/e80051e55f.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jockey+One&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../public/assets/header_footer.css">

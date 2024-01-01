5bb4e414b025ca67ba2f91f86d1027de6f781191
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-01 21:03:34 +0100

    Reduce code

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -11,4 +11,8 @@ class HomeController extends Controller
     public function notFound(){
         $this->render('404');
     }
+
+    public function dashboard() {
+        $this->render('admin/dashboard');
+    }
 }
